17e7f10a58fb7ac0cda7bc8ded928c19aa504c42
Mahmoud Nabhan <niypoo@gmail.com>
2023-10-16 12:41:30 +0300

    Update timeout.widget.dart

--- lib/widgets/timeout.widget.dart
+++ lib/widgets/timeout.widget.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/widgets.dart';
 import 'package:fly_ui/views/widgets/images/image.widget.dart';
 import 'package:get/get.dart';
@@ -11,15 +10,23 @@ class Timeout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const FlyImage(
-            url: 'assets/images/characters/pain.svg',
-            height: 150,
-            width: 150,
-          ),
-          Text('Something went wrong'.tr),
-        ],
-      );
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const FlyImage(
+          url: 'assets/images/characters/pain.svg',
+          height: 150,
+          width: 150,
+        ),
+        Text(
+          'Something went wrong'.tr,
+          style: Get.textTheme.bodyLarge,
+        ),
+        const SizedBox(height: 5),
+        Text(
+          'Try closing the application and opening it again.'.tr,
+          style: Get.textTheme.bodyMedium,
+        ),
+      ],
+    );
   }
 }
