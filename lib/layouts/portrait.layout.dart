f0da80549e76b1e030252a9223642e4b605e1579
Mahmoud Nabhan <niypoo@gmail.com>
2023-10-16 12:25:12 +0300

    add timeout issue catch

--- lib/layouts/portrait.layout.dart
+++ lib/layouts/portrait.layout.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/widgets.dart';
+import 'package:fly_splash_module/splash.controller.dart';
 import 'package:fly_splash_module/widgets/dedication.widget.dart';
 import 'package:fly_splash_module/widgets/loading.widget.dart';
+import 'package:fly_ui/views/widgets/images/image.widget.dart';
+import 'package:get/get.dart';
 
-class PortraitLayout extends StatelessWidget {
+class PortraitLayout extends GetView<SplashController> {
   const PortraitLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        LoadingWidget(),
-        Dedication(),
+        const Spacer(),
+        Obx(
+          () => controller.issued.isFalse
+              ? const LoadingWidget()
+              : Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const FlyImage(
+                      url: '/assets/images/characters/pain.svg',
+                      height: 200,
+                      width: 200,
+                    ),
+                    Text('Something went wrong'.tr),
+                  ],
+                ),
+        ),
+        const Spacer(),
+        const Dedication(),
       ],
     );
   }
--- lib/splash.controller.dart
+++ lib/splash.controller.dart
@@ -3,4 +3,27 @@ import 'package:get/get.dart';
 class SplashController extends GetxController {
   // controller
   static SplashController get to => Get.find();
+
+  // define timeout
+  late Future timeout;
+  // timeout duration in //minutes
+  late int timeoutDuration = 2;
+  // notify issue found
+  RxBool issued = RxBool(false);
+
+  @override
+  void onInit() {
+    timeout = Future.delayed(
+      Duration(minutes: timeoutDuration),
+      () {
+        issued.value = true;
+      },
+    );
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
 }
